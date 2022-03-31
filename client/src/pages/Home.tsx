1a3885cf6f77d76ed73a39c6bebd2e4786d18e82
kykim0613 <97214073+kykim0613@users.noreply.github.com>
2022-03-31 17:11:04 +0900

    home2

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,11 +1,13 @@
 import Header from "../components/Header";
 import HeaderMenu from "../components/HeaderMenu";
+import Slider from "../components/Slider";
 
 const Home = () => {
   return (
     <>
     <HeaderMenu />
     <Header />
+    <Slider />
     </>
   );
 };
