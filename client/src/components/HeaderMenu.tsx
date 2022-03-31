df92850e5774fe3fd821c94023da3c0c88bb1276
kykim0613 <97214073+kykim0613@users.noreply.github.com>
2022-03-31 15:01:08 +0900

    Home ver1.0

---
+++ client/src/components/HeaderMenu.tsx
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+const Container = styled.div`
+    display: flex;
+    -webkit-box-pack: center;
+    justify-content: center;
+    border-bottom: 1px solid rgb(238, 238, 238);
+    height: 40px;
+    background: rgb(255, 255, 255);
+`
+const HeaderMenu =  () => {
+    return (
+        <Container></Container>
+    )
+}
+
+export default HeaderMenu;
