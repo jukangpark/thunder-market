632725764bbac0aa67e3cdb7781cd88630f82217
jukangpark <skyxxx9339@naver.com>
2022-04-05 13:05:31 +0900

    aws 업데이트

--- server/src/middleware/middlewares.ts
+++ server/src/middleware/middlewares.ts
@@ -1,6 +1,23 @@
 const multer = require("multer");
+import multerS3 from "multer-s3";
+import aws from "aws-sdk";
 
-export const uploadProduct = multer({
+const s3 = new aws.S3({
+  credentials: {
+    accessKeyId: `${process.env.AWS_ID}`,
+    secretAccessKey: `${process.env.AWS_SECRET}`,
+  },
+});
+
+const isHeroku = process.env.NODE_ENV === "production";
+
+const s3ProductUploader = multerS3({
+  s3: s3,
+  bucket: "thunder-market/product",
+  acl: "public-read",
+});
+
+export const uploadProductImage = multer({
   dest: "uploads/product",
-  storage: undefined,
+  storage: isHeroku ? s3ProductUploader : undefined,
 });
